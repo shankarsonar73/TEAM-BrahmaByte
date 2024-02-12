7f023041f071f437d4a9c5854d1f0ca9c12b5d38
Shankar Sonar <125757038+shankarsonar73@users.noreply.github.com>
2024-02-12 19:32:12 +0530

    Update tzkt.jsx

--- client/src/utils/tzkt.jsx
+++ client/src/utils/tzkt.jsx
@@ -1,10 +1,8 @@
-// TODO 8 - Fetch storage of the Lottery by completing fetchStorage
-
 import axios from "axios";
 
 export const fetchStorage = async () => {
     const res = await axios.get(
-        "https://api.ghostnet.tzkt.io/v1/contracts/KT1WT6AjrzNhUqXZgc2JVE8bvRgb9WmkUXrT/storage"
+        "https://api.ghostnet.tzkt.io/v1/contracts/KT1TNE7pfAggMKxFqpa5gjQYPYB6nnkdzcAX/storage"
     );
     return res.data;
 };
